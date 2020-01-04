79e605603f9add4225a3148f5d84a42968174af6
Garrett Hoch <garretthoch@gmail.com>
2020-01-04 11:55:25 -0500

    got ball working

--- pong/main/game.py
+++ pong/main/game.py
@@ -7,6 +7,8 @@ from .. import socketio
 
 from . import main
 
+import time
+
 
 @main.route('/play', methods=('GET', 'POST'))
 def play():
@@ -15,3 +17,41 @@ def play():
 @socketio.on('test')
 def connect(json):
     emit('response', json['test'])
+
+
+@socketio.on('moveup')
+def moveup(json):
+    emit('response', json['direction'])
+
+@socketio.on('movedown')
+def movedown(json):
+    emit('response', json['direction'])
+
+@socketio.on('startgame')
+def startgame(message):
+    ydir=1#positive=right
+    xdir=1 #positive=up
+    speed=20
+    height=message['gameh']
+    width=message['gamew']
+    xball=width/2
+    yball=height/2
+    lball = 10
+    while(True):
+        if xball <= 0:
+            xdir=1
+        if xball >= width-lball:
+            xdir = -1
+        if yball <= 0:
+            ydir=1
+        if yball >= height-lball:
+            ydir = -1
+        
+        xball+=(xdir*speed)
+        yball+=(ydir*speed)
+        #xball +=xdir
+        #yball +=ydir
+        emit('moveball',{'x':xball, 'y':yball, 'l':lball})
+        time.sleep(.5)
+
+
--- pong/static/js/draw.js
+++ pong/static/js/draw.js
@@ -7,11 +7,22 @@ var x =0;
 var y =0;
 var wid = 15;
 var hei=75;
+var xball = canvas.width/2
+var yball = canvas.height/2
+var lball = 10;
+
+var start = true
 
 
 function drawPaddle(x,y,wid,hei){
     ctx.fillStyle='black'
     ctx.fillRect(x,y,wid,hei)
+
+}
+
+function drawball(x,y,len){
+    ctx.fillStyle='black'
+    ctx.fillRect(x,y,len,len)
 
 }
 
@@ -21,9 +32,11 @@ function move(){
     
     if(up && y>0){
         y=y-5
+        socket.emit('moveup', {'direction':"Move up"});
     }
     if(down && y<canvas.height-hei){
         y=y+5
+        socket.emit('movedown', {'direction':"Move Down"});
     }
 }
 var up = false;
@@ -31,7 +44,6 @@ var down = false;
 
 window.onkeydown = function(e) {
     var keyPr = e.code;
-    console.log(keyPr)
     if (e.code == "ArrowUp"){ //upkey
         up=true;
     }
@@ -41,7 +53,6 @@ window.onkeydown = function(e) {
 }
 window.onkeyup = function(e) {
     var keyPr = e.code;
-    console.log(keyPr)
     if (e.code == "ArrowUp") //upkey
         up=false;
     if (e.code == "ArrowDown") //downkey
@@ -53,26 +64,44 @@ function clearcanvas(){
 }
 
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-    socket.on('connect', () =>{
-        socket.emit('test', {'test':"helloworld"});
-    });
 
-    socket.on('response', message =>{
-        document.querySelector('.test').innerHTML= message;
-    })
-
-    requestAnimationFrame(update);
+var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+socket.on('connect', () =>{
+    socket.emit('test', {'test':"helloworld"});
 });
 
-//setInterval(update,10)
+socket.on('response', message =>{
+    document.querySelector('.test').innerHTML= message;
+});
+
+socket.on('moveball',message =>{
+    xball=message['x']
+    yball=message['y']
+    lball=message['l']
+    console.log("ball moved")
+})
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('start').onclick=function(){
+        socket.emit('startgame',{'gameh':canvas.height,'gamew':canvas.width});
+        requestAnimationFrame(update);
+    };
+    
+});
+
+
+
+
 
 
 function update(){
     clearcanvas();
     drawPaddle(x,y,wid,hei);
     move();
+    drawball(xball,yball,lball)
     requestAnimationFrame(update);
     
 }
